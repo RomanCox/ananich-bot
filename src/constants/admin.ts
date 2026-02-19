3916224ad90e5e25756b5c9cacbff4b865c12579
Roman Cox <rom.cox.dev@gmail.com>
2026-02-19 17:29:59 +0300

    add cpanel.yaml

--- src/constants/admin.ts
+++
@@ -1,7 +0,0 @@
-import { AdminStep } from "../types/navigation";
-
-export const AdminBackMap: Record<AdminStep, AdminStep | null> = {
-	main: null,
-	users_list: "main",
-	upload_xlsx: "main",
-};
--- src/constants/catalog.ts
+++
@@ -1,7 +0,0 @@
-import { FilteringProducts } from "../types/navigation";
-
-export const CatalogBackMap: Record<FilteringProducts, FilteringProducts | null> = {
-	brands: null,
-	categories: "brands",
-	products: "categories",
-};
